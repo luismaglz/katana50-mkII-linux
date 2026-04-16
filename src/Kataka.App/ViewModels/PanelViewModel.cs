045b1d4991764cd80877670539d86c692171bcd0
Luis Gonzalez <luismaglz@live.com>
2026-04-15 23:36:19 -0600

    add PanelView and PanelViewModel for amp control panel UI, update MainWindow to use unified amp state refresh

---
+++ src/Kataka.App/ViewModels/PanelViewModel.cs
@@ -0,0 +1,35 @@
+using Kataka.Domain.KatanaState;
+
+namespace Kataka.App.ViewModels;
+
+public class PanelViewModel : ViewModelBase
+{
+    public PanelViewModel(IKatanaState katanaState)
+    {
+        AmpType = new AmpControlViewModel(katanaState.AmpType);
+        AmpVariation = new AmpControlViewModel(katanaState.AmpVariation);
+        Gain = new AmpControlViewModel(katanaState.Gain);
+        Volume = new AmpControlViewModel(katanaState.Volume);
+        Bass = new AmpControlViewModel(katanaState.Bass);
+        Middle = new AmpControlViewModel(katanaState.Middle);
+        Treble = new AmpControlViewModel(katanaState.Treble);
+        Presence = new AmpControlViewModel(katanaState.Presence);
+        CabinetResonance = new AmpControlViewModel(katanaState.CabinetResonance);
+        PedalChain = new AmpControlViewModel(katanaState.PedalChain);
+    }
+
+    #region AmpControls
+
+    public AmpControlViewModel AmpType { get; internal set; }
+    public AmpControlViewModel AmpVariation { get; internal set; }
+    public AmpControlViewModel Gain { get; internal set; }
+    public AmpControlViewModel Volume { get; internal set; }
+    public AmpControlViewModel Bass { get; internal set; }
+    public AmpControlViewModel Middle { get; internal set; }
+    public AmpControlViewModel Treble { get; internal set; }
+    public AmpControlViewModel Presence { get; internal set; }
+    public AmpControlViewModel CabinetResonance { get; internal set; }
+    public AmpControlViewModel PedalChain { get; internal set; }
+
+    #endregion
+}
