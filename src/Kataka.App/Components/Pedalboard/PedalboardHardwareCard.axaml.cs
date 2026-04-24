314eb9431bfba4a18c7c8bbb14468bf135090e9a
Luis Gonzalez <luismaglz@live.com>
2026-04-23 22:52:00 -0600

    Add PedalboardHardwareCard control for reusable input/output/amp UI; refactor PedalboardView to use it in data templates

---
+++ src/Kataka.App/Components/Pedalboard/PedalboardHardwareCard.axaml.cs
@@ -0,0 +1,85 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Kataka.App.Components.Pedalboard;
+
+public partial class PedalboardHardwareCard : UserControl
+{
+    public static readonly StyledProperty<bool> ShowConnectorProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, bool>(nameof(ShowConnector));
+
+    public static readonly StyledProperty<string> ImagePathProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, string>(nameof(ImagePath), string.Empty);
+
+    public static readonly StyledProperty<string> LabelProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, string>(nameof(Label), string.Empty);
+
+    public static readonly StyledProperty<IBrush> AccentBrushProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, IBrush>(nameof(AccentBrush), Brushes.Gray);
+
+    public static readonly StyledProperty<IBrush> CardBorderBrushProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, IBrush>(nameof(CardBorderBrush), Brushes.Gray);
+
+    public static readonly StyledProperty<IBrush> LabelBrushProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, IBrush>(nameof(LabelBrush), Brushes.White);
+
+    public static readonly StyledProperty<double> CardWidthProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, double>(nameof(CardWidth), 100);
+
+    public static readonly StyledProperty<double> LabelFontSizeProperty =
+        AvaloniaProperty.Register<PedalboardHardwareCard, double>(nameof(LabelFontSize), 11);
+
+    public PedalboardHardwareCard()
+    {
+        InitializeComponent();
+    }
+
+    public bool ShowConnector
+    {
+        get => GetValue(ShowConnectorProperty);
+        set => SetValue(ShowConnectorProperty, value);
+    }
+
+    public string ImagePath
+    {
+        get => GetValue(ImagePathProperty);
+        set => SetValue(ImagePathProperty, value);
+    }
+
+    public string Label
+    {
+        get => GetValue(LabelProperty);
+        set => SetValue(LabelProperty, value);
+    }
+
+    public IBrush AccentBrush
+    {
+        get => GetValue(AccentBrushProperty);
+        set => SetValue(AccentBrushProperty, value);
+    }
+
+    public IBrush CardBorderBrush
+    {
+        get => GetValue(CardBorderBrushProperty);
+        set => SetValue(CardBorderBrushProperty, value);
+    }
+
+    public IBrush LabelBrush
+    {
+        get => GetValue(LabelBrushProperty);
+        set => SetValue(LabelBrushProperty, value);
+    }
+
+    public double CardWidth
+    {
+        get => GetValue(CardWidthProperty);
+        set => SetValue(CardWidthProperty, value);
+    }
+
+    public double LabelFontSize
+    {
+        get => GetValue(LabelFontSizeProperty);
+        set => SetValue(LabelFontSizeProperty, value);
+    }
+}
