1386ee976bf23a4c6193d227571bd2bff3294d55
Luis Gonzalez <luismaglz@live.com>
2026-04-23 22:45:47 -0600

    Refactor pedalboard modeling: simplify PedalboardPosition hierarchy, update PedalboardView and ViewModel to use strongly-typed chain nodes and data templates

--- src/Kataka.App/Components/Pedalboard/PedalboardPosition.cs
+++ src/Kataka.App/Components/Pedalboard/PedalboardPosition.cs
@@ -1,43 +1,22 @@
-// #region Copyright
-//
-// =================================================================================================
-// This file is part of the Navitaire software development kit.
-// Copyright © Navitaire LLC  An Amadeus company. All rights reserved.
-// ==================================================================================================
-//
-// #endregion
-
 using Kataka.App.ViewModels;
 
 namespace Kataka.App.Components.Pedalboard;
 
-public abstract class PedalboardPosition
+public abstract class PedalboardPosition(string color)
 {
-    protected PedalboardPosition(string color)
-    {
-        Color = color;
-    }
-
-    public string Color { get; }
+    public string Color { get; } = color;
 }
 
-public class PedalboardHardware : PedalboardPosition
-{
-    public PedalboardHardware(string imagePath, string color)
-        : base(color)
-    {
-        ImagePath = imagePath;
-    }
+public class PedalboardInput(string color) : PedalboardPosition(color);
 
-    public string ImagePath { get; set; }
+public class PedalboardOutput(string color) : PedalboardPosition(color);
+
+public class PedalboardAmp(string color) : PedalboardPosition(color);
+
+public abstract class PedalboardPedalBase(ViewModelBase viewModel, string color) : PedalboardPosition(color)
+{
+    public ViewModelBase ViewModel { get; } = viewModel;
 }
 
-public class PedalboardPedal<T> : PedalboardPosition where T : ViewModelBase
-{
-    public PedalboardPedal(T viewModel, string color) : base(color)
-    {
-        ViewModel = viewModel;
-    }
-
-    public T ViewModel { get; set; }
-}
+public class PedalboardPedal<T>(T viewModel, string color) : PedalboardPedalBase(viewModel, color)
+    where T : ViewModelBase;
